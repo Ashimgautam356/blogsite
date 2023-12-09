ced7546fb799cb287be0b1ae85728624a0afeb34
Ashim Gautam <computer77356@gmail.com>
2023-12-09 21:35:14 +0545

    login page UI done

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -2,7 +2,26 @@ import React from 'react'
 
 const Login = () => {
   return (
-    <div className='bg-[rgb(247,120,4)]'>Login</div>
+    <div className='bg-[rgb(247,120,4)] relative flex flex-row justify-center items-center w-full h-screen'>
+
+    <div className='flex flex-col md:w-2/4 sm:w-5/6 relative items-center border border-white text-white md:text-2xl sm:text-lg font-medium rounded-lg'>
+        <h1 className='text-white text-3xl font-medium text-center m-10'>Signup!</h1>
+        <form action="/signup" method='POST' className='sm:w-full'>
+            <div className='mb-6'>
+                <label htmlFor="userName">UserName:-   </label>
+                <input type='text' className='focus:outline-none bg-[rgb(247,120,4)] border border-white rounded-md placeholder-gray-300 p-2 text-gray-200 font-light sm:w-3/6' placeholder='Tom' required></input>
+            </div>
+            <div className='mb-6'>
+                <label htmlFor="password">Password:-   </label>
+                <input type='password' className='focus:outline-none bg-[rgb(247,120,4)] border border-white rounded-md placeholder-gray-300 p-2 text-gray-200 font-light sm:w-4/6' placeholder='tom@gmail.com' required></input>
+            </div>
+          
+            <div className='mb-6 text-center'>
+                <input type='submit'  className='focus:outline-none bg-[rgb(247,120,4)] border border-white rounded-md p-2 cursor-pointer'></input>
+            </div>
+        </form>
+    </div>
+</div>
   )
 }
 
--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -4,31 +4,31 @@ const Signup = () => {
   return (
     <div className='bg-[rgb(247,120,4)] relative flex flex-row justify-center items-center w-full h-screen'>
 
-        <div className='flex flex-col w-2/4 relative items-center border border-white text-white text-2xl font-medium rounded-lg'>
+        <div className='flex flex-col md:w-2/4 sm:w-5/6 relative items-center border border-white text-white md:text-2xl sm:text-xl font-medium rounded-lg'>
             <h1 className='text-white text-3xl font-medium text-center m-10'>Signup!</h1>
-            <form action="/signup" method='POST'>
-                <div className='mb-6'>
+            <form action="/signup" method='POST' className='sm:w-full'>
+                <div className='mb-6 sm:w-full'>
                     <label htmlFor="email">Email:-   </label>
-                    <input type='email' className='focus:outline-none bg-[rgb(247,120,4)] border border-white rounded-md placeholder-gray-200 p-2 text-gray-200 font-light' placeholder='tom@gmail.com' required></input>
+                    <input type='email' className='focus:outline-none bg-[rgb(247,120,4)] border border-white rounded-md placeholder-gray-300 p-2  text-white font-light sm:w-4/6' placeholder='tom@gmail.com' required></input>
                 </div>
                 <div className='mb-6'>
                     <label htmlFor="userName">UserName:-   </label>
-                    <input type='text' className='focus:outline-none bg-[rgb(247,120,4)] border border-white rounded-md placeholder-gray-200 p-2 text-gray-200 font-light' placeholder='Tom' required></input>
+                    <input type='text' className='focus:outline-none bg-[rgb(247,120,4)] border border-white rounded-md placeholder-gray-300 p-2 text-gray-200 font-light sm:w-4/6' placeholder='Tom' required></input>
                 </div>
                 <div className='mb-6'>
                     <label htmlFor="password">Password:-   </label>
-                    <input type='password' className='focus:outline-none bg-[rgb(247,120,4)] border border-white rounded-md placeholder-gray-200 p-2 text-gray-200 font-light' placeholder='tom@gmail.com' required></input>
+                    <input type='password' className='focus:outline-none bg-[rgb(247,120,4)] border border-white rounded-md placeholder-gray-300 p-2 text-gray-200 font-light sm:w-4/6' placeholder='tom@gmail.com' required></input>
                 </div>
                 <div className='mb-6'>
-                    <label htmlFor="confirmPassword">Confirm Password:-   </label>
-                    <input type='password' className='focus:outline-none bg-[rgb(247,120,4)] border border-white rounded-md placeholder-gray-200 p-2 text-gray-200 font-light' placeholder='tom@gmail.com' required></input>
+                    <label htmlFor="confirmPassword">Confirm:-   </label>
+                    <input type='password' className='focus:outline-none bg-[rgb(247,120,4)] border border-white rounded-md placeholder-gray-300 p-2 text-gray-200 font-light sm:w-4/6' placeholder='tom@gmail.com' required></input>
                 </div>
                 <div className='mb-6'>
                     <label htmlFor="confirmPassword">Profile  </label>
-                    <input type='file' className='focus:outline-none bg-[rgb(247,120,4)] border border-white rounded-md placeholder-gray-200 p-2 text-gray-200 font-light'></input>
+                    <input type='file' id='profilePic' name='profilePic' accept=".jpg, .png, .jpeg" className='focus:outline-none bg-[rgb(247,120,4)] border border-white rounded-md placeholder-gray-200 p-2 text-gray-200 font-light sm:w-4/6'></input>
                 </div>
                 <div className='mb-6 text-center'>
-                    <input type='submit' className='focus:outline-none bg-[rgb(247,120,4)] border border-white rounded-md p-2 cursor-pointer'></input>
+                    <input type='submit'  className='focus:outline-none bg-[rgb(247,120,4)] border border-white rounded-md p-2 cursor-pointer'></input>
                 </div>
             </form>
         </div>
