f5f175cf920c8833bfba14dde4a27e208ed2f055
Ashim Gautam <computer77356@gmail.com>
2023-12-22 23:05:56 +0545

    only post which are associated with gear are managed in their own components

--- frontend/src/components/home/Gear.jsx
+++ frontend/src/components/home/Gear.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import React, { useContext,useState,useEffect } from 'react'
 import test65 from '../../images/test65.jpg'
 import { Link } from 'react-router-dom'
+import {AuthContext} from '../../context/authContext'
+
 
 
 const Gear = () => {
+
+    const { posts } = useContext(AuthContext);
+    const [gearPosts, setGearPosts] = useState([]);
+  
+    useEffect(() => {
+      const fetchPosts = async () => {
+        try {
+          const fetchedPosts = await posts();
+          setGearPosts(fetchedPosts);
+        } catch (error) {
+          console.error('Error fetching posts:', error);
+        }
+      };
+  
+      fetchPosts();
+    }, [posts]);
+
+    // rending only the gear posts 
+    const g = gearPosts.filter(post => post.cate === 'gear' )
+
   return (
     <div className=' flex flex-col sm:w-5/6 p-5 w-4/6 relative h-full'>
         <div className='w-full font-medium text-3xl mb-4 h-5/6'>
@@ -13,50 +35,32 @@ const Gear = () => {
         </div>
         <div className='flex md:flex-row sm:flex-col w-full justify-between h-full '>
             <div className='sm:w-full w-3/5 relative sm:mb-10 min-h-full'>
-                <h1 className='absolute z-10 font-semibold text-4xl text-white top-3/4 sm:text-xl pl-2' >This is the heading of the image</h1>
-                <img src={test65} alt="" className='w-full min-h-full object-cover object-center brightness-75'/>
+                <h1 className='absolute z-10 font-semibold text-4xl text-white top-3/4 sm:text-xl pl-2'>
+                    {g[0].heading}
+                </h1>
+                <img src={`../upload/${g[0].photo}`} alt="" className='w-full min-h-full object-cover object-center brightness-75' />
             </div>
-            <div className='flex flex-col sm:w-full w-2/6 '>
-                <Link className='flex flex-row w-full   justify-between mb-10'>
-                    <div className='w-3/5 min-h-full  '>
-                        <img src={test65} alt=""  className='h-full'/>
-                    </div>
-                    <div className='w-2/6'>
-                        <h2 className='mb-3 text-red-800 font-semibold'>
-                            <Link>News</Link>
-                        </h2>
-                        <h1 className='text-sm'>
-                            <Link>This is the heading of the pic</Link>
-                        </h1>
-                    </div>
-                </Link>
-                <Link className='flex flex-row  w-full   justify-between mb-10'>
-                    <div className='w-3/5 min-h-full  '>
-                        <img src={test65} alt=""  className='h-full'/>
-                    </div>
-                    <div className='w-2/6 '>
-                        <h2 className='mb-3 text-red-800 font-semibold'>
-                            <Link>News</Link>
-                        </h2>
-                        <h1 className='text-sm'>
-                            <Link>This is the heading of the pic</Link>
-                        </h1>
-                    </div>
-                </Link>
-                <Link className='flex flex-row  w-full   justify-between'>
-                    <div className='w-3/5 min-h-full  '>
-                        <img src={test65} alt=""  className='h-full'/>
-                    </div>
-                    <div className='w-2/6'>
-                        <h2 className='mb-3 text-red-800 font-semibold'>
-                            <Link>News</Link>
-                        </h2>
-                        <h1 className='text-sm'>
-                            <Link>This is the heading of the pic</Link>
-                        </h1>
-                    </div>
-                </Link>
-            </div>
+            <div className= 'flex flex-col sm:w-full w-2/6'>
+                {
+                    g.map((post,index)=>{
+                       return (index !== 0 ?
+                            <Link className='flex flex-row w-full justify-between mb-10'>
+                                <div className='w-3/5 min-h-full'>
+                                    <img src={`../upload/${post.photo}`} alt="" className='h-full' />
+                                </div>
+                                <div className='w-2/6'>
+                                    <h2 className='mb-3 text-red-800 font-semibold'>
+                                    <Link>News</Link>
+                                    </h2>
+                                    <h1 className='text-sm'>
+                                    <Link>{post.heading}</Link>
+                                    </h1>
+                                </div>
+                            </Link>
+                        : "")
+                    })
+                }
+            </div>         
         </div>
     </div>
   )
